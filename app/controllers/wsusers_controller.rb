b4a9996b3dd42658bd610c70aa94be3d27333e06
Andres Felipe Sanchez Lemus <andfsanchezlem@unal.edu.co>
2017-11-01 21:03:51 -0500

    function make soap

--- app/controllers/wsusers_controller.rb
+++ app/controllers/wsusers_controller.rb
@@ -1,6 +1,15 @@
 class WsusersController < ApplicationController
   soap_service namespace: 'urn:WashOutUser', camelize_wsdl: :lower
 
+  # make case
+  soap_action "make",
+              :args   => { :firstName => :string, :lastName => :string, :email => :string, :password => :string },
+              :return => :boolean
+  def make
+    operation = User.create(firstName: params[:firstName], lastName: params[:lastName],email: params[:email], password: params[:password])
+    render :soap => true
+  end
+
   soap_action "check",
               :args   => { :email => :string},
               :return => :boolean
